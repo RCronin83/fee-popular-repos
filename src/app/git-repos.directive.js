4317f3f23d4f5cd52484dd0a7059af1837d04b76
Ryan Cronin <Ryan@Ryans-MBP.home>
2017-04-11 19:28:02 -0400

    added doc blocks and panel collapse function

--- src/app/git-repos.directive.js
+++ src/app/git-repos.directive.js
@@ -3,7 +3,10 @@
 
   angular.module('gh')
     .directive('gitRepo', gitRepo);
-
+    /**
+     * creates a custom directive
+     * @return {object} options for our directive
+     */
     function gitRepo(){
       let $ = angular.element;
 
@@ -11,11 +14,18 @@
         templateUrl: 'repos/single-repo.template.html',
         restrict: 'E',
         link: setupCollapse,
-        transclude; true,
+        transclude: true,
         scope:{
           repo: '='
+
         }
       };
+      /**
+       * allows for collapsable panels
+       * @param  {Object}           scope   properties of our directive
+       * @param  {HTMLInputElement} element The element which we apply the function to
+       * @return {Void}         [description]
+       */
       function setupCollapse(scope, element){
         $(element)
           .find('header')
