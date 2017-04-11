a1f54798c20dd5036e232aaee35cfe3269a42192
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-11 14:10:41 -0400

    added collapsable panels

--- src/app/git-repos.directive.js
+++ src/app/git-repos.directive.js
@@ -5,12 +5,23 @@
     .directive('gitRepo', gitRepo);
 
     function gitRepo(){
+      let $ = angular.element;
+
       return{
         templateUrl: 'repos/single-repo.template.html',
         restrict: 'E',
+        link: setupCollapse,
+        transclude; true,
         scope:{
           repo: '='
         }
       };
+      function setupCollapse(scope, element){
+        $(element)
+          .find('header')
+          .on('click', function hidePanelBody(){
+            $(element).find('section').toggleClass('hidden');
+          });
+      }
     }
 }());
