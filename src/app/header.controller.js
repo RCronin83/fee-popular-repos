d69d04fecd8110f838bbdd79e4d385ef313f2c77
Ryan Cronin <Ryan@Ryans-MBP.home>
2017-04-11 19:32:30 -0400

    added doc block

--- src/app/header.controller.js
+++ src/app/header.controller.js
@@ -5,6 +5,10 @@
         .controller('HeaderController', HeaderController);
 
     HeaderController.$inject = ['GithubService'];
+    /**
+     * gets and saves our authentication token
+     * @param {function} GithubService service which connects to github api
+     */
     function HeaderController(GithubService) {
         let vm = this;
         vm.token = GithubService.getUserToken() || '';
