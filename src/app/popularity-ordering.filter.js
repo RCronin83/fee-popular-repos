37bc1151965ca849a17c89ce6cf2a597f0bf9616
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-12 09:41:30 -0400

    updated popularity function

--- src/app/popularity-ordering.filter.js
+++ src/app/popularity-ordering.filter.js
@@ -22,9 +22,7 @@
       * @type {Number}
       */
       return collectionCopy.sort(function doPopularitySorting(a, b){
-        let popularityA = ((a.stargazers_count) + (2 * (a.forks_count)) + ((1/2) * (a.open_issues_count)));
-        let popularityB = ((b.stargazers_count) + (2 * (b.forks_count)) + ((1/2) * (b.open_issues_count)));
-        return popularityB - popularityA;
+        return b.popularity - a.popularity;
       });
 
     };
